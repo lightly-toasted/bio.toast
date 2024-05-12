8e5dd434d1a0c2b808db788945bc8241045e8a02
toast <toastlua@gmail.com>
2024-05-12 14:36:40 +0900

    update redirect

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,5 +2,5 @@ import type { Load } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 
 export const load: Load = async () => {
-    throw redirect(307, '/lightly-toasted')
+    throw redirect(307, 'https://github.com/lightly-toasted/bio.toast')
 }
