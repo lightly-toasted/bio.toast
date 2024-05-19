38dbc50d364d18c2e5396385f110520de8359107
toast <toastlua@gmail.com>
2024-05-19 21:53:12 +0900

    Set favicon

--- src/routes/[name]/+page.svelte
+++ src/routes/[name]/+page.svelte
@@ -35,6 +35,7 @@
     <meta property="og:description" content="bio.toast.name/{data.github.login} {data.config.description || data.github.bio}" />
     <meta property="og:image" content="{data.config.embedImage}" />
     <meta property="og:type" content="website" />
+    <link rel="icon" href="{data.github.avatar_url}" />
     <title>{data.github.name ? `${data.github.name} (@${data.github.login})` : `@${data.github.login}`}</title>
 </svelte:head>
 
